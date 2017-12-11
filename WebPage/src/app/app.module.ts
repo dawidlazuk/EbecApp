04ca9de9c4ef1e56e7fc9cc68c946fe0d0812438
Dawid Lazuk <dawid.lazuk@accenture.com>
2017-12-11 08:42:11 +0100

    Frontend moved to folder & time-counter component

--- WebPage/src/app/app.module.ts
+++ WebPage/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TimeCounterComponent } from './shared/time-counter/time-counter.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeCounterComponent
   ],
   imports: [
     BrowserModule
---
+++ WebPage/src/app/shared/time-counter/time-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-counter',
+  templateUrl: './time-counter.component.html',
+  styleUrls: ['./time-counter.component.css']
+})
+export class TimeCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
